e754d79c27500f3549a80488b202bdabcd037df6
OlegCherniakov <ol-cherniakov@yandex.ru>
2020-04-16 16:48:18 +0300

    Добавляем ввод с клавиатуры

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function renderAdded() {
   const noSongsElement = container.querySelector(".no-songs");
 
   if (songs.length === 0) {
-    resetButton.setAttribute("disabled", true);
-    resetButton.classList.add("input__btn_disabled");
+    resetButton.setAttribute("disabled", true);                       // здесь уберём атрибут "disabled"
+    resetButton.classList.add("input__btn_disabled");                 // а здесь уберём класс "input__btn_disabled"
     noSongsElement.classList.remove("no-songs_hidden");
   } else {
     resetButton.removeAttribute("disabled");
@@ -20,14 +20,18 @@ function renderAdded() {
 
 function addSong() {
   const artist = document.querySelector(".input__text_artist");
-  const song = document.querySelector(".input__text_song");
+  const song = document.querySelector(".input__text_song");// аналогично выберите input с названием песни
+  //Управление содержимым: свойства .innerHTML / .textContent
+  /* вставим разметку с помощью innerHTML
+    Используйте грависы, чтобы разместить её
+    на нескольких строчках */
 
-  const trackContainer = document.createElement("div");
-  trackContainer.classList.add("song");
+  const trackContainer = document.createElement("div");/* здесь создайте элемент div */
+  trackContainer.classList.add("song");                   /* а здесь присвойте ему класс .song */
 
-  const artistElement = document.createElement("h4");
-  artistElement.classList.add("song__artist");
-  artistElement.textContent = artist.value;
+  const artistElement = document.createElement("h4");      // создаём элемент h4 и кладём его в переменную
+  artistElement.classList.add("song__artist");             // добавляем класс song__artist
+  artistElement.textContent = artist.value;                // добавляем текст
 
   const titleElement = document.createElement("p");
   titleElement.classList.add("song__title");
@@ -52,6 +56,13 @@ function addSong() {
 
   renderAdded();
 }
+
+const artistInput = document.querySelector('.input__text_artist');         //Ввод с клавиатуры + Enter в поле исполнитель
+artistInput.addEventListener('keydown', function (event) {
+  if  (event.key === 'Enter') {
+    addSong(event);                                                        //В функцию добавления песни в плейлист добавляем ввод с клавиатуры
+  }
+});
 
 function resetPlaylist() {
   songsContainer.innerHTML = "";
