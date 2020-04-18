0afb57fab224586c2bb1d3856fcfedf765609f4e
OlegCherniakov <ol-cherniakov@yandex.ru>
2020-04-18 13:21:59 +0300

    Приводим код в человеческий вид

--- script.js
+++ script.js
@@ -1,144 +1,116 @@
-const container = document.querySelector(".container");
-const songsContainer = container.querySelector(".songs-container");
-const addButton = container.querySelector(".input__btn_add");
-const resetButton = container.querySelector(".input__btn_reset");
+/* Переменные */
 
-function renderAdded() {
-  const songs = songsContainer.querySelectorAll(".song");
-  const noSongsElement = container.querySelector(".no-songs");
-
-  if (songs.length === 0) {
-    resetButton.setAttribute("disabled", true); // здесь уберём атрибут "disabled"
-    resetButton.classList.add("input__btn_disabled"); // а здесь уберём класс "input__btn_disabled"
-    noSongsElement.classList.remove("no-songs_hidden");
-  } else {
-    resetButton.removeAttribute("disabled");
-    resetButton.classList.remove("input__btn_disabled");
-    noSongsElement.classList.add("no-songs_hidden");
-  }
-}
-
-// функция принимает два параметра
-function createSong(artistValue, songValue) {
-  // выносим логику создания элемента из функции addSong
-  const trackContainer = document.createElement("div");
-  trackContainer.classList.add("song");
-
-  const artistElement = document.createElement("h4");
-  artistElement.classList.add("song__artist");
-  artistElement.textContent = artistValue; // первый параметр используем здесь
-
-  const titleElement = document.createElement("p");
-  titleElement.classList.add("song__title");
-  titleElement.textContent = songValue; // а второй здесь
-
-  const songButtonElement = document.createElement("button");
-  songButtonElement.classList.add("song__like");
-
-  trackContainer.appendChild(artistElement);
-  trackContainer.appendChild(titleElement);
-  trackContainer.appendChild(songButtonElement);
-
-  // необходимо вернуть элемент трека
-  return trackContainer;
-}
-
-// функция принимает два параметра
-function createSong(artistValue, songValue) {
-  // выносим логику создания элемента из функции addSong
-  const trackContainer = document.createElement("div");
-  trackContainer.classList.add("song");
-
-  const artistElement = document.createElement("h4");
-  artistElement.classList.add("song__artist");
-  artistElement.textContent = artistValue; // первый параметр используем здесь
-
-  const titleElement = document.createElement("p");
-  titleElement.classList.add("song__title");
-  titleElement.textContent = songValue; // а второй здесь
-
-  const songButtonElement = document.createElement("button");
-  songButtonElement.classList.add("song__like");
-
-  trackContainer.appendChild(artistElement);
-  trackContainer.appendChild(titleElement);
-  trackContainer.appendChild(songButtonElement);
-
-  // необходимо вернуть элемент трека
-  return trackContainer;
-}
-
-function addSong(event) {
-  event.preventDefault();
-
-  const form = document.forms.add; // в эту переменную запишите форму
-  const artist = form.elements.artist; //достаньте элемент по имени artist
-  const song = form.elements.song; //а этот по имени song
-
-  // вызываем функцию createSong, передавай ей аргументы
-  const trackContainer = createSong(artist.value, song.value);
-
-  // дальше ничего не изменилось
-  songsContainer.appendChild(trackContainer);
-
-  document.forms.add.reset(); //обнуление (очистка) форм заполнения после ввода
-
-  renderAdded();
-
-  addButton.setAttribute("disabled", true);
-  addButton.classList.add("input__btn_disabled");
-}
-
-const artistInput = document.querySelector(".input__text_artist"); //Ввод с клавиатуры в поле
-const songInput = document.querySelector(".input__text_song");
-
-function resetPlaylist() {
-  songsContainer.innerHTML = "";
-
-  renderAdded();
-}
-
-addButton.addEventListener("click", addSong); //Слушатели
-resetButton.addEventListener("click", resetPlaylist); //Слушатели
-
-renderAdded();
+const container = document.querySelector('.container');
+const songsContainer = container.querySelector('.songs-container');
+const addButton = container.querySelector('.input__btn_add');
+const resetButton = container.querySelector('.input__btn_reset');
+const coverHeading = container.querySelector('.cover__heading');
+const form = document.forms.add;
 
 const playListTitles = [
-  "Игорь Тальков. Лучшее",
-  "Музыка категории Б",
-  "Подборка с фестиваля FYRE",
+    'Игорь Тальков. Лучшее',
+    'Музыка категории Б',
+    'Подборка с фестиваля FYRE'
 ];
 
-function doubleClickHandler(event) {
-  event.target.textContent =
-    playListTitles[Math.floor(Math.random() * playListTitles.length)];
-  document
-    .querySelector(".cover__heading")
-    .removeEventListener("dblclick", doubleClickHandler); //Снимаем слушателя. Двойной клик будет один раз
+/* Функции */
+
+// Перерисовывает блок добавленных песен
+function renderAdded() {
+    const songs = songsContainer.querySelectorAll('.song');
+    const noSongsElement = container.querySelector('.no-songs');
+
+    if (songs.length === 0) {
+        resetButton.setAttribute('disabled', true);
+        resetButton.classList.add('input__btn_disabled');
+        noSongsElement.classList.remove('no-songs_hidden');
+    } else {
+        resetButton.removeAttribute('disabled');
+        resetButton.classList.remove('input__btn_disabled');
+        noSongsElement.classList.add('no-songs_hidden');
+    }
 }
 
-document
-  .querySelector(".cover__heading")
-  .addEventListener("dblclick", doubleClickHandler); //Слушатель, который запускает по двойному клику function doubleClickHandler
+// Создаёт элемент песни и возвращает его
+function createSong(artistValue, songValue) {
+    const trackContainer = document.createElement('div');
+    const artistElement = document.createElement('h4');
+    const titleElement = document.createElement('p');
+    const songButtonElement = document.createElement('button');
 
-// вешаем слушатель на элемент songsContainer
-songsContainer.addEventListener("click", function (event) {
-  event.target.classList.toggle("song__like_active");
-  if (event.target.classList.contains("song__like")) {
-    event.target.classList.toggle("song__like_active");
-  }
-});
+    trackContainer.classList.add('song');
+    artistElement.classList.add('song__artist');
+    artistElement.textContent = artistValue;
+    titleElement.classList.add('song__title');
+    titleElement.textContent = songValue;
+    songButtonElement.classList.add('song__like');
 
-const form = document.forms.add; //обработчик изменения формы (события input).
-const artist = form.elements.artist;
-const song = form.elements.song;
+    trackContainer.appendChild(artistElement);
+    trackContainer.appendChild(titleElement);
+    trackContainer.appendChild(songButtonElement);
 
-form.addEventListener("input", function (event) {
-  if (artist.value.length === 0 || song.value.length === 0) {
-    addButton.setAttribute("disabled", true);
-    addButton.classList.add("input__btn_disabled");
-  } else {
-    addButton.removeAttribute("disabled");
-    addButton.classList.remove("input__btn_disabled");
-  }
-});
+    return trackContainer;
+}
+
+// Добавляет элемент песни
+function addSong(event) {
+    event.preventDefault();
+
+    const artist = form.elements.artist;
+    const song = form.elements.song;
+    const trackContainer = createSong(artist.value, song.value);
+
+    songsContainer.appendChild(trackContainer);
+    form.reset();
+    renderAdded();
+    addButton.setAttribute('disabled', true);
+    addButton.classList.add('input__btn_disabled');
+}
+
+// Обработчик клика по кнопке «Очистить плейлист»
+function resetPlaylist() {
+    songsContainer.innerHTML = '';
+    renderAdded();
+}
+
+// Обработчик двойного клика
+function doubleClickHandler(event) {
+    event.target.textContent = playListTitles[
+        Math.floor(Math.random() * playListTitles.length)
+    ];
+
+    coverHeading.removeEventListener('dblclick', doubleClickHandler);
+}
+
+// Обработчик события input
+function inputHandler() {
+    const artist = event.currentTarget.elements.artist;
+    const song = event.currentTarget.elements.song;
+
+    if (artist.value.length === 0 || song.value.length === 0) {
+        addButton.setAttribute('disabled', true);
+        addButton.classList.add('input__btn_disabled');
+    } else {
+        addButton.removeAttribute('disabled');
+        addButton.classList.remove('input__btn_disabled');
+    }
+}
+
+// обработчик клика по сердечку
+function likeHandler(event) {
+    if (event.target.classList.contains('song__like')) {
+        event.target.classList.toggle('song__like_active');
+    }
+}
+
+/* Слушатели событий */
+
+resetButton.addEventListener('click', resetPlaylist);
+coverHeading.addEventListener('dblclick', doubleClickHandler);
+songsContainer.addEventListener('click', likeHandler);
+form.addEventListener('input', inputHandler);
+form.addEventListener('submit', addSong);
+
+/* Вызовы функций */
+
+renderAdded();
